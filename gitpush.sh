1057a5683a6729ecadcdd5a27d14249b4c22d403
mitsui1028 <37976262+mitsui19841028@users.noreply.github.com>
2021-03-20 21:03:37 +0900

    first commit

---
+++ gitpush.sh
@@ -0,0 +1,6 @@
+echo "# php-book-app" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git remote add origin https://github.com/mitsui19841028/cakephpPractice.git
+git push -u origin master
